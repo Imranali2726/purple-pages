2e932e33f91adf67a60abc7e0429aca5e3968848
NabeelAhmed23 <nabeel.ahmed@mtp.tech>
2022-08-23 11:21:09 +0500

    Changed image src of sliders

--- fakeData/homepage.js
+++ fakeData/homepage.js
@@ -1,27 +1,27 @@
 export const slider1 = [
-  { text: "ELA Training Services, UK", img: "https://picsum.photos/344/220" },
+  { text: "ELA Training Services, UK", img: "/images/img5.jpg" },
   {
     text: "Small Steps Big Dreams, Dubai",
-    img: "https://picsum.photos/345/220",
+    img: "/images/img5.jpg",
   },
-  { text: "Trainings & Courses", img: "https://picsum.photos/346/220" },
-  { text: "Oxford University, England", img: "https://picsum.photos/347/220" },
+  { text: "Trainings & Courses", img: "/images/img5.jpg" },
+  { text: "Oxford University, England", img: "/images/img5.jpg" },
 ];
 export const slider2 = [
-  { text: "Sports", img: "https://picsum.photos/348/220" },
-  { text: "Retail", img: "https://picsum.photos/349/220" },
-  { text: "Travel", img: "https://picsum.photos/341/220" },
-  { text: "Restaurants", img: "https://picsum.photos/342/220" },
+  { text: "Sports", img: "/images/img5.jpg" },
+  { text: "Retail", img: "/images/img5.jpg" },
+  { text: "Travel", img: "/images/img5.jpg" },
+  { text: "Restaurants", img: "/images/img5.jpg" },
 ];
 export const slider3 = [
-  { text: "Hospitals", img: "https://picsum.photos/343/220" },
-  { text: "DayCare", img: "https://picsum.photos/344/220" },
-  { text: "Nurses", img: "https://picsum.photos/345/220" },
-  { text: "Pharmaceutical", img: "https://picsum.photos/346/220" },
+  { text: "Hospitals", img: "/images/img5.jpg" },
+  { text: "DayCare", img: "/images/img5.jpg" },
+  { text: "Nurses", img: "/images/img5.jpg" },
+  { text: "Pharmaceutical", img: "/images/img5.jpg" },
 ];
 export const slider4 = [
-  { text: "Adult Care Worker", img: "https://picsum.photos/347/220" },
-  { text: "Health and social care", img: "https://picsum.photos/348/220" },
-  { text: "Leader in adult care", img: "https://picsum.photos/349/220" },
-  { text: "Early years Educator", img: "https://picsum.photos/340/220" },
+  { text: "Adult Care Worker", img: "/images/img5.jpg" },
+  { text: "Health and social care", img: "/images/img5.jpg" },
+  { text: "Leader in adult care", img: "/images/img5.jpg" },
+  { text: "Early years Educator", img: "/images/img5.jpg" },
 ];
