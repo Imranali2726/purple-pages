085b6f743d44cc990caac01262d7e3178fb71859
NabeelAhmed23 <nabeel.ahmed@mtp.tech>
2022-11-02 01:04:58 +0500

    change

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    BASE_URL_UAT: "http://153.92.210.69/index.php/api/",
+    BASE_URL_UAT: "http://153.92.210.69/api/",
     BASE_URL_LOCAL: "http://localhost:8000/api/",
-    BASE_UAT_SERVER: "http://153.92.210.69/index.php/",
+    BASE_UAT_SERVER: "http://153.92.210.69/",
   },
 };
 
