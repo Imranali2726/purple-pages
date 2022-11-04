460a1c1a332017f2be66b8cf29f8e83e09c3f0d5
NabeelAhmed23 <nabeel.ahmed@mtp.tech>
2022-11-04 11:30:09 +0500

    changes

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    BASE_URL_UAT: "http://153.92.210.69/api/",
+    BASE_URL_UAT: "http://purplepages.io/api/",
     BASE_URL_LOCAL: "http://localhost:8000/api/",
-    BASE_UAT_SERVER: "http://153.92.210.69/",
+    BASE_UAT_SERVER: "http://purplepages.io/",
   },
 };
 
