fb1d90c457b21290048a0a680a8d1ec6b53ed6bd
NabeelAhmed23 <nabeel.ahmed@mtp.tech>
2022-11-13 21:40:26 +0500

    sdfsd

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  trailingSlash: true,
   env: {
-    BASE_URL_UAT: "http://purplepages.io/api/",
+    BASE_URL_UAT: "https://purplepages.io/api/",
     BASE_URL_LOCAL: "http://localhost:8000/api/",
-    BASE_UAT_SERVER: "http://purplepages.io/",
+    BASE_UAT_SERVER: "https://purplepages.io/",
     BASE_LOCAL_SERVER: "http://localhost:8000/",
   },
 };
