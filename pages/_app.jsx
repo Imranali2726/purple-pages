84553cd249a57447b32ec50514d3f43f06dcc021
NabeelAhmed23 <nabeel.ahmed@mtp.tech>
2022-11-14 23:23:55 +0500

    added accessibility tool

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import LoadingBar from "react-top-loading-bar";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import Script from "next/script";
 import { Provider } from "react-redux";
 import { SessionProvider } from "next-auth/react";
 import Layout from "./layout";
@@ -21,43 +22,29 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 
   return (
     <>
-      {/* <Script id="accessbi" strategy="lazyOnload">
-        {` (function(){
-        var s    = document.createElement('script');
-        var h    = document.querySelector('head') || document.body;
-        s.src    = 'https://acsbapp.com/apps/app/dist/js/app.js';
-        s.async  = true;
-        s.onload = function(){
-            acsbJS.init({
-                statementLink    : '',
-                footerHtml       : '',
-                hideMobile       : false,
-                hideTrigger      : false,
-                disableBgProcess : false,
-                language         : 'en',
-                position         : 'right',
-                leadColor        : '#146FF8',
-                triggerColor     : '#146FF8',
-                triggerRadius    : '50%',
-                triggerPositionX : 'right',
-                triggerPositionY : 'bottom',
-                triggerIcon      : 'people',
-                triggerSize      : 'bottom',
-                triggerOffsetX   : 20,
-                triggerOffsetY   : 20,
-                mobile           : {
-                    triggerSize      : 'small',
-                    triggerPositionX : 'right',
-                    triggerPositionY : 'bottom',
-                    triggerOffsetX   : 20,
-                    triggerOffsetY   : 20,
-                    triggerRadius    : '20'
-                }
-            });
-        };
-        h.appendChild(s);
-    })();`}
-      </Script> */}
+      <Script id="accessbi" strategy="lazyOnload">
+        {`  (function(d){
+         var s = d.createElement("script");
+         /* uncomment the following line to override default position*/
+         /* s.setAttribute("data-position", 3);*/
+         /* uncomment the following line to override default size (values: small, large)*/
+         /* s.setAttribute("data-size", "small");*/
+         /* uncomment the following line to override default language (e.g., fr, de, es, he, nl, etc.)*/
+         /* s.setAttribute("data-language", "language");*/
+         /* uncomment the following line to override color set via widget (e.g., #053f67)*/
+         /* s.setAttribute("data-color", "#053e67");*/
+         /* uncomment the following line to override type set via widget (1=person, 2=chair, 3=eye, 4=text)*/
+         /* s.setAttribute("data-type", "1");*/
+         /* s.setAttribute("data-statement_text:", "Our Accessibility Statement");*/
+         /* s.setAttribute("data-statement_url", "http://www.example.com/accessibility")";*/
+         /* uncomment the following line to override support on mobile devices*/
+         /* s.setAttribute("data-mobile", true);*/
+         /* uncomment the following line to set custom trigger action for accessibility menu*/
+         /* s.setAttribute("data-trigger", "triggerId")*/
+         s.setAttribute("data-account", "LMBzsbZJ1G");
+         s.setAttribute("src", "https://cdn.userway.org/widget.js");
+         (d.body || d.head).appendChild(s);})(document)`}
+      </Script>
       <Provider store={store}>
         <SessionProvider session={session}>
           <Layout>
