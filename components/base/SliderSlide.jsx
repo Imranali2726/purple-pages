2c9d6f2fc546e982bc1a626b0b4a228981859295
NabeelAhmed23 <nabeel.ahmed@mtp.tech>
2022-11-14 22:58:15 +0500

    changes in featured slider

--- components/base/SliderSlide.jsx
+++ components/base/SliderSlide.jsx
@@ -13,7 +13,7 @@ export default function SliderSlide({ text, img, slug }) {
             className="w-full min-h-[220px] max-h-[220px] object-cover object-center"
           />
           <div className="border border-t-none pt-8 px-5 pb-3">
-            <h3 className="font-bold text-sm md:text-base lg:text-lg h-[56px]">
+            <h3 className="font-bold text-sm md:text-base lg:text-lg min-h-[56px]">
               {text}
             </h3>
           </div>
